dd8de1706acb3f396805099f2024323c8fca29df
Alejandro Gabriel <gbrayhan@gmail.com>
2019-09-17 18:46:44 -0500

    Se integra los servicios de lint de validacion de codigo

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-"use strict";
+'use strict'
 
-const {graphql, buildSchema} = require('graphql');
-const express = require('express');
-const gqlMiddleware = require('express-graphql');
+const { buildSchema } = require('graphql')
+const express = require('express')
+const gqlMiddleware = require('express-graphql')
 
-const app = express();
-const port = process.env.port || 3000;
-
+const app = express()
+const port = process.env.port || 3000
 
 // Definimos el esquema inicial
 const schema = buildSchema(`
@@ -16,28 +15,27 @@ const schema = buildSchema(`
         "Retorna un saludo generico" 
         saludo: String
     }
- `);
+ `)
 
 // Configurar los resolvers
 const resolvers = {
-    hello: () => {
-        return 'Hola Mundo'
-    },
-    saludo: () => {
-        return 'Hola a Todos'
-    }
-};
-
+  hello: () => {
+    return 'Hola Mundo'
+  },
+  saludo: () => {
+    return 'Hola a Todos'
+  }
+}
 
 app.use('/app', gqlMiddleware({
-    schema: schema,
-    rootValue: resolvers,
-    graphiql: true
-}));
+  schema: schema,
+  rootValue: resolvers,
+  graphiql: true
+}))
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}/app `)
-});
+  console.log(`Server is listening at http://localhost:${port}/app `)
+})
 
 // Se ejecuta el query Hello y saludo
 // graphql(schema, '{ hello, saludo }', resolvers).then((data) => {
